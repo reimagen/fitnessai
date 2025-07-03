c7d5f2f9c25adc1ea1a941fa03d53f21443a1085
Lisa G <whereisgu22@gmail.com>
2025-07-03 22:41:40 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['var(--font-inter)', 'sans-serif'],
-        headline: ['var(--font-inter)', 'sans-serif'],
+        body: ['var(--font-pt-sans)', 'sans-serif'],
+        headline: ['var(--font-pt-sans)', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
