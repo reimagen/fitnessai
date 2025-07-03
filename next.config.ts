52f3551c87750d5984d833ef34543e4ea7243c28
Lisa G <whereisgu22@gmail.com>
2025-07-03 17:35:12 +0000

    i have deleted src/next.config.ts can you double check the configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: ['https://*.cloudworkstations.dev'],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
