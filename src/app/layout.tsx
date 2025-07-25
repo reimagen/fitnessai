a1cbe32459ab31b8d85a193836f12ff109feda0b
Lisa G <whereisgu22@gmail.com>
2025-07-25 20:43:25 +0000

    you keep re-adding the (app) files when everything has been moved to src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { QueryProvider } from '@/components/query-provider';
 import { ThemeProvider } from '@/components/theme-provider';
+import { BottomNavigationBar } from '@/components/layout/bottom-navigation-bar';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -72,7 +73,8 @@ export default function RootLayout({
         >
           <QueryProvider>
             <div className="flex min-h-screen flex-col">
-              {children}
+              <main className="flex-grow pb-20 pt-4">{children}</main>
+              <BottomNavigationBar />
             </div>
           </QueryProvider>
           <Toaster />
