e35bfa60e851463d8e838b93507ffeee1bb14b14
Lisa G <whereisgu22@gmail.com>
2025-07-28 18:46:24 +0000

    proceed

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -7,17 +8,28 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-sm font-medium leading-none",
+  {
+    variants: {
+      variant: {
+        default: "peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        file: "",
+      }
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
 )
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
     VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
+>(({ className, variant, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={cn(labelVariants(), className)}
+    className={cn(labelVariants({ variant }), className)}
     {...props}
   />
 ))
