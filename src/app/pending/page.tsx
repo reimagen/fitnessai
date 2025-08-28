85969b0d0b0999195c5814f25c07f8eaf06b7067
Lisa G <whereisgu22@gmail.com>
2025-08-28 23:11:52 +0000

    it's unnecessary to have 2 sections for thank you/to request access. mer

--- src/app/pending/page.tsx
+++ src/app/pending/page.tsx
@@ -4,14 +4,14 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { useAuth } from "@/lib/auth.service";
-import { LogOut, Send } from "lucide-react";
+import { Send } from "lucide-react";
 import Link from "next/link";
 
 // Form for non-whitelist attempted signups
 const ACCESS_REQUEST_FORM_URL = "https://forms.fillout.com/t/3TWB1xQnJLus";
 
 export default function PendingAccessPage() {
-  const { signOut, user } = useAuth();
+  const { user } = useAuth();
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background px-4">
@@ -24,21 +24,16 @@ export default function PendingAccessPage() {
                 <CardHeader>
                     <CardTitle className="font-headline text-2xl">Access Restricted</CardTitle>
                     <CardDescription>
-                        Thank you for your interest! Access to this application is currently limited to approved users.
+                        Thank you for your interest! Access to this application is currently limited to approved users. To request access, please fill out the form below.
                     </CardDescription>
                 </CardHeader>
-                <CardContent className="space-y-6">
-                    <p className="text-sm text-muted-foreground">
-                        To request access, please fill out the form below. If your request is approved, your account for <span className="font-semibold text-primary">{user?.email}</span> will be enabled.
-                    </p>
-                    <div className="space-y-6">
-                        <Link href={ACCESS_REQUEST_FORM_URL} target="_blank" passHref>
-                            <Button className="w-full bg-accent hover:bg-accent/90 text-accent-foreground">
-                                <Send className="mr-2" />
-                                Request Access
-                            </Button>
-                        </Link>
-                    </div>
+                <CardContent>
+                    <Link href={ACCESS_REQUEST_FORM_URL} target="_blank" passHref>
+                        <Button className="w-full bg-accent hover:bg-accent/90 text-accent-foreground">
+                            <Send className="mr-2" />
+                            Request Access
+                        </Button>
+                    </Link>
                 </CardContent>
             </Card>
         </div>
