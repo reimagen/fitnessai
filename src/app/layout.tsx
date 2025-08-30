c398ddfd93326fb023d6c4f21a1878fbd301f80e
Lisa G <whereisgu22@gmail.com>
2025-08-30 17:53:01 +0000

    what is the solution?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,9 +49,6 @@ export const metadata: Metadata = {
     },
     description: APP_DESCRIPTION,
   },
-  icons: {
-    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>ð</text></svg>",
-  }
 };
 
 export const viewport: Viewport = {
