f1d7d7018598c973874c3e16dce3cd0e1836f82b
Lisa G <whereisgu22@gmail.com>
2025-08-30 17:51:28 +0000

    i'm not seeing my fave icon. can you audit why?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,9 @@ export const metadata: Metadata = {
     },
     description: APP_DESCRIPTION,
   },
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>ð</text></svg>",
+  }
 };
 
 export const viewport: Viewport = {
